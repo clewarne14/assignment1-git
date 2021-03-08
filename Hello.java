0f22e5f85d39cf26538802a8702733111dda3a47
clewarne14 <clewarne15@gmail.com>
2021-03-07 22:43:13 -0800

    Added a new line stating that this was to simulate a merge conflict

--- Hello.java
+++ Hello.java
@@ -4,5 +4,6 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello World!");
     System.out.println("\nOutput for  a failed push");
+    System.out.println("\nOutput for a failed merge")
   }
 }
