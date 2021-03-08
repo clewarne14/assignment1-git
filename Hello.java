2f8d8eb2e15bbfb0b5420467720a529c6dffe280
clewarne14 <clewarne15@gmail.com>
2021-03-07 22:51:11 -0800

    I missed a semicolon in the code, I am updating it

--- Hello.java
+++ Hello.java
@@ -4,6 +4,6 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello World!");
     System.out.println("\nOutput for  a failed push");
-    System.out.println("\nOutput for a failed merge")
+    System.out.println("\nOutput for a failed merge");
   }
 }
