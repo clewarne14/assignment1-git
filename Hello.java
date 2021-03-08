025a2ed1f945467e6bce176fc42557874337646a
clewarne14 <clewarne15@gmail.com>
2021-03-07 22:59:43 -0800

    Removed the line that mentions the failed merge

--- Hello.java
+++ Hello.java
@@ -4,6 +4,5 @@ public class Hello{
   public static void main(String[] args){
     System.out.println("Hello World!");
     System.out.println("\nOutput for  a failed push");
-    System.out.println("\nOutput for a failed merge");
   }
 }
